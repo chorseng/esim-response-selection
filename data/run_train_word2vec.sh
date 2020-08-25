c2eb33e718206290b399ec03550708ba227d2a7d
chorseng <39493602+chorseng@users.noreply.github.com>
2020-08-25 18:11:09 +0800

    Update run_train_word2vec.sh

--- data/run_train_word2vec.sh
+++ data/run_train_word2vec.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
-python train_word2vec.py ./ubuntu_data/train.txt ./embedding_w2v_d300.txt
+python train_word2vec.py /home/chorseng/IOI/data/ubuntu1/train.txt ./embedding_w2v_d300.txt
+
+# was ./ubuntu_data/train.txt
